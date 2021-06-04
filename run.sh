ddbbd2b72ea511aaef1659fa338746540822b054
Youplala <eliebrosset@gmail.com>
2021-06-04 17:38:33 +0200

    readme and run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-LANG="FR"
-DATA_DIR=default_data/disciplines/
+if [ $# -eq 0 ]
+  then
+    echo "Please give EN of FR as argument. Example: ./run.sh EN"
+    exit
+fi
+
+LANG="$1"
+DATA_DIR=default_data/disciplines/$LANG
 echo number of discipline files is:
 ls "$DATA_DIR" | wc -l
-FILES=default_data/disciplines/$LANG/*
-for file in $FILES
+
+for file in $DATA_DIR/*
 do
 echo current file location is $file
 filename=$(basename "$file" | cut -d. -f1)
 echo current file is "$filename"
 
-docker run -d -v "$PWD"/data:/autophrase/data -v "$PWD"/models:/autophrase/models -it\
+docker run -d -v "/${PWD}/data":/autophrase/data -v "/${PWD}/models":/autophrase/models -it\
     --name autophrase \
     -e RAW_TRAIN=${file} \
     -e ENABLE_POS_TAGGING=1 \
@@ -19,9 +25,13 @@ docker run -d -v "$PWD"/data:/autophrase/data -v "$PWD"/models:/autophrase/model
     -e TEXT_TO_SEG=${file} \
     remenberl/autophrase
 echo docker container created.
+if [ $LANG = 'FR' ]
+  then
+    docker cp default_data/disciplines autophrase:/autophrase/default_data/
+    docker cp data/$LANG autophrase:/autophrase/default_data/
+    docker cp data/$LANG/french.par autophrase:/autophrase/tools/treetagger/lib/french-utf8.par
+fi
 docker cp default_data/disciplines autophrase:/autophrase/default_data/
-docker cp data/$LANG autophrase:/autophrase/default_data/
-docker cp data/$LANG/french.par autophrase:/autophrase/tools/treetagger/lib/french-utf8.par
 docker exec -it autophrase ls default_data
 docker exec -it autophrase ./auto_phrase.sh
 docker exec -it autophrase ls models
