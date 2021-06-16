f29eef78cda5d644879f362c6404dc36e5ccc61d
Youplala <eliebrosset@gmail.com>
2021-06-16 19:23:48 +0200

    windows or linux

--- run.sh
+++ run.sh
@@ -1,57 +1,125 @@
 #!/bin/bash
 
-# Check arguments
-if [ $# -eq 0 ]
-  then
-    echo "Please give EN of FR as argument. Example: ./run.sh EN"
-    exit
-fi
 
-LANG="$1"
-DATA_DIR=default_data/disciplines/$LANG
-echo number of discipline files is:
-ls "$DATA_DIR" | wc -l
-
-# Loop through available text files
-for file in $DATA_DIR/*
-  do
-    echo current file location is $file
-    # Extract filename without .txt extension
-    filename=$(basename "$file" | cut -d. -f1)
-    echo current file is "$filename"
-
-    # Run docker container using custom data, pos tagging and segmentation
-    winpty docker run -d -v "/${PWD}/data":/autophrase/data -v "/${PWD}/models":/autophrase/models -it\
-        --name autophrase \
-        -e RAW_TRAIN=${file} \
-        -e ENABLE_POS_TAGGING=1 \
-        -e MIN_SUP=30 -e THREAD=10 \
-        -e MODEL=models/$LANG/"$filename" \
-        -e TEXT_TO_SEG=${file} \
-        remenberl/autophrase
-    echo docker container created.
-
-    docker cp default_data/disciplines autophrase:/autophrase/default_data/
-
-    if [ $LANG = 'FR' ]
+case "$OSTYPE" in
+  linux*)   echo "detected OS is LINUX" 
+  
+    # Check arguments
+    if [ $# -eq 0 ]
       then
-        # Copy French txt files to autophrase default data
-        docker cp data/$LANG autophrase:/autophrase/default_data/
-        # Add special french library to treetagger
-        docker cp data/$LANG/french.par autophrase:/autophrase/tools/treetagger/lib/french-utf8.par
+        echo "Please give EN of FR as argument. Example: ./run.sh EN"
+        exit
     fi
-    
-    # Execute autophrase
-    winpty docker exec -it autophrase ./auto_phrase.sh
 
-    # Export output models to our computer
-    docker cp autophrase:/autophrase/models/$LANG/"$filename" models/$LANG/
+    LANG="$1"
+    DATA_DIR=default_data/disciplines/$LANG
+    echo number of discipline files is:
+    ls "$DATA_DIR" | wc -l
 
-    # Stop and delete docker container
-    docker container stop autophrase
-    docker container rm autophrase
-    echo quality phrases of "$filename" generated.
+    # Loop through available text files
+    for file in $DATA_DIR/*
+      do
+        echo current file location is $file
+        # Extract filename without .txt extension
+        filename=$(basename "$file" | cut -d. -f1)
+        echo current file is "$filename"
 
-  done
+        # Run docker container using custom data, pos tagging and segmentation
+        sudo docker run -d -v "/${PWD}/data":/autophrase/data -v "/${PWD}/models":/autophrase/models -it\
+            --name autophrase \
+            -e RAW_TRAIN=${file} \
+            -e ENABLE_POS_TAGGING=1 \
+            -e MIN_SUP=30 -e THREAD=10 \
+            -e MODEL=models/$LANG/"$filename" \
+            -e TEXT_TO_SEG=${file} \
+            remenberl/autophrase
+        echo docker container created.
 
-echo script executed.
+        docker cp default_data/disciplines autophrase:/autophrase/default_data/
+
+        if [ $LANG = 'FR' ]
+          then
+            # Copy French txt files to autophrase default data
+            docker cp data/$LANG autophrase:/autophrase/default_data/
+            # Add special french library to treetagger
+            docker cp data/$LANG/french.par autophrase:/autophrase/tools/treetagger/lib/french-utf8.par
+        fi
+        
+        # Execute autophrase
+        sudo docker exec -it autophrase ./auto_phrase.sh
+
+        # Export output models to our computer
+        docker cp autophrase:/autophrase/models/$LANG/"$filename" models/$LANG/
+
+        # Stop and delete docker container
+        docker container stop autophrase
+        docker container rm autophrase
+        echo quality phrases of "$filename" generated.
+
+      done
+
+    echo script executed.;;
+
+
+
+
+
+  msys*)    echo "detected OS is WINDOWS"
+  
+    # Check arguments
+    if [ $# -eq 0 ]
+      then
+        echo "Please give EN of FR as argument. Example: ./run.sh EN"
+        exit
+    fi
+
+    LANG="$1"
+    DATA_DIR=default_data/disciplines/$LANG
+    echo number of discipline files is:
+    ls "$DATA_DIR" | wc -l
+
+    # Loop through available text files
+    for file in $DATA_DIR/*
+      do
+        echo current file location is $file
+        # Extract filename without .txt extension
+        filename=$(basename "$file" | cut -d. -f1)
+        echo current file is "$filename"
+
+        # Run docker container using custom data, pos tagging and segmentation
+        winpty docker run -d -v "/${PWD}/data":/autophrase/data -v "/${PWD}/models":/autophrase/models -it\
+            --name autophrase \
+            -e RAW_TRAIN=${file} \
+            -e ENABLE_POS_TAGGING=1 \
+            -e MIN_SUP=30 -e THREAD=10 \
+            -e MODEL=models/$LANG/"$filename" \
+            -e TEXT_TO_SEG=${file} \
+            remenberl/autophrase
+        echo docker container created.
+
+        docker cp default_data/disciplines autophrase:/autophrase/default_data/
+
+        if [ $LANG = 'FR' ]
+          then
+            # Copy French txt files to autophrase default data
+            docker cp data/$LANG autophrase:/autophrase/default_data/
+            # Add special french library to treetagger
+            docker cp data/$LANG/french.par autophrase:/autophrase/tools/treetagger/lib/french-utf8.par
+        fi
+        
+        # Execute autophrase
+        winpty docker exec -it autophrase ./auto_phrase.sh
+
+        # Export output models to our computer
+        docker cp autophrase:/autophrase/models/$LANG/"$filename" models/$LANG/
+
+        # Stop and delete docker container
+        docker container stop autophrase
+        docker container rm autophrase
+        echo quality phrases of "$filename" generated.
+
+      done
+
+    echo script executed.;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
